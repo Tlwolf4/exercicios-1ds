71dab26ca92e3a4a48db7c8975bc45b8ad2d1c87
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2025-10-14 14:35:44 +0000

    concluido

--- 4-bimestre/ex03-loop-for/index.js
+++ 4-bimestre/ex03-loop-for/index.js
@@ -1,4 +1,22 @@
 // CRIE UMA LÓGICA AQUI
+resultado = 55
+for (let resultado = 1; resultado < 55; resultado = resultado + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 ) {
+    console.log(`${resultado}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // === NÃO MODIFIQUE OU ADICIONE NADA ABAIXO ============
 module.exports = resultado
