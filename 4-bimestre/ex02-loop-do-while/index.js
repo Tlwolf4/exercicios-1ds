b2721ebc3b9a8eab1a2270e9fd8aedb648f66506
Eryck fernando <eryck.barbosa@aluno.ce.gov.br>
2025-10-14 13:40:41 +0000

    concluido

--- 4-bimestre/ex02-loop-do-while/index.js
+++ 4-bimestre/ex02-loop-do-while/index.js
@@ -1,4 +1,15 @@
 // CRIE UMA LÓGICA AQUI
+let resultado = 5
+
+do {
+    console.log(`${resultado}`)
+    resultado = resultado + 1
+
+} while (resultado < 10) 
+    console.log("Tentando acessar")
+
+console.log(resultado)
+
 
 // === NÃO MODIFIQUE NADA ABAIXO ============
 module.exports = resultado
